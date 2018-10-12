b50e8052bd904bda67be650af04e08ddc0405170
Alex Gleason <alex@alexgleason.me>
2018-10-12 13:45:36 -0400

    Fix import issue on newer Wagtail versions

--- wagalytics/wagtail_hooks.py
+++ wagalytics/wagtail_hooks.py
@@ -1,26 +1,38 @@
-from django.conf.urls import include, url
-from django.core import urlresolvers
 from django.conf import settings
-from wagtail.wagtailcore import hooks
-from wagtail.wagtailadmin.menu import MenuItem
 from django.utils.translation import ugettext_lazy as _
-from django.core.urlresolvers import reverse
+
+try:
+    from wagtail.core import hooks
+    from wagtail.admin.menu import MenuItem
+except ImportError:  # fallback for Wagtail <2.0
+    from wagtail.wagtailcore import hooks
+    from wagtail.wagtailadmin.menu import MenuItem
+
+try:
+    from django.urls import re_path, include
+except ImportError:  # fallback for Django <2.0
+    from django.conf.urls import url as re_path
+    from django.conf.urls import include
+
+try:
+    from django.urls import reverse
+except ImportError:  # fallback for Django <1.9
+    from django.core.urlresolvers import reverse
 
 from . import urls
-from . import views
 
 
 @hooks.register('register_admin_urls')
 def register_admin_urls():
     return [
-        url(r'^analytics/', include(urls)),
+        re_path(r'^analytics/', include(urls)),
     ]
 
 @hooks.register('register_admin_menu_item')
 def register_styleguide_menu_item():
     return MenuItem(
         _('Analytics'),
-        urlresolvers.reverse('wagalytics_dashboard'),
+        reverse('wagalytics_dashboard'),
         classnames='icon icon-fa-bar-chart',
         order=1000
     )
